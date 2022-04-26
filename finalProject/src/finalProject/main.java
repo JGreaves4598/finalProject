2d4b356cda4f838b99376a1cf3839286930b7fb6
Jack Greaves <JGreaves4598@gmail.com>
2022-04-26 10:11:36 +0100

    create view function

--- finalProject/src/finalProject/main.java
+++ finalProject/src/finalProject/main.java
@@ -4,37 +4,38 @@ import java.util.ArrayList;
 
 public class main {
 	
-
-	
-
 	public static void main(String[] args) {
 		ArrayList<String> userlist = new ArrayList<String>();
 		userlist.add("John");
 		userlist.add("Barry");
 		userlist.add("Tom");
-		
-		Scanner input = new Scanner(System.in);
-		System.out.print("What would you like to do? Add, View, Update or Delete: ");
-		String option = input.next();
-		
-		if(option.equalsIgnoreCase("Add")) {
-			System.out.println("Adding new user...");
-			System.out.println("Enter new user name here:");
-			String name = input.next();
-			userlist.add(name);
-			System.out.println(name+" has been added.");
+		while(true) {
+			Scanner input = new Scanner(System.in);
+			System.out.print("What would you like to do? Add, View, Update or Delete: ");
+			String option = input.next();
 			
-		}else if(option.equalsIgnoreCase("View")) {
-			System.out.println("View");
-		}else if(option.equalsIgnoreCase("Update")) {
-			System.out.println("Update");
-		}else if(option.equalsIgnoreCase("Delete")) {
-			System.out.println("Delete");
-		}else {
-			System.out.println("You must enter a valid option");
+			if(option.equalsIgnoreCase("Add")) {
+				System.out.println("Adding new user...");
+				System.out.print("Enter new user name here:");
+				String name = input.next();
+				userlist.add(name);
+				System.out.println(name+" has been added.");
+				
+			}else if(option.equalsIgnoreCase("View")) {
+				System.out.println("Viewing list of users...");
+				for(int i=0;i<userlist.size();i++) {
+					String item = userlist.get(i);
+					System.out.println(item+" | id: "+i);
+				}	
+				
+			}else if(option.equalsIgnoreCase("Update")) {
+				System.out.println("Update");
+			}else if(option.equalsIgnoreCase("Delete")) {
+				System.out.println("Delete");
+			}else {
+				System.out.println("You must enter a valid option");
+			}
 		}
-		main(null);
-		
 	}
 
 }
